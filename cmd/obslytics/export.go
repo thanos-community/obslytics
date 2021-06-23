2accc241cfbbe55576dbacf262ebc777c6eb2a5b
Anish Asthana <anishasthana1@gmail.com>
2021-06-23 08:11:16 -0400

    Add Profiling Code
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- cmd/obslytics/export.go
+++ cmd/obslytics/export.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"runtime"
 	"time"
 
 	"github.com/go-kit/kit/log"
@@ -21,6 +22,19 @@ import (
 	exportertfactory "github.com/thanos-community/obslytics/pkg/exporter/factory"
 	infactory "github.com/thanos-community/obslytics/pkg/series/factory"
 )
+
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
 
 func registerExport(m map[string]setupFunc, app *kingpin.Application) {
 	cmd := app.Command("export", "Export observability series data into popular analytics formats.")
@@ -44,29 +58,43 @@ func registerExport(m map[string]setupFunc, app *kingpin.Application) {
 	m["export"] = func(g *run.Group, logger log.Logger) error {
 		ctx, cancel := context.WithCancel(context.Background())
 		g.Add(func() error {
+			fmt.Println("Before Parsing Matchers")
+			PrintMemUsage()
 			matchers, err := parser.ParseMetricSelector(*matchersStr)
 			if err != nil {
 				return errors.Wrap(err, "parsing provided matchers")
 			}
 
+			fmt.Println("Matchers Parsed")
+			PrintMemUsage()
 			inputCfg, err := inputFlag.Content()
 			if err != nil {
 				return err
 			}
+
+			fmt.Println("Input config determined")
+			PrintMemUsage()
 			in, err := infactory.NewSeriesReader(logger, inputCfg)
 			if err != nil {
 				return err
 			}
+
+			fmt.Println("Series reader created")
+			PrintMemUsage()
 			outputCfg, err := outputFlag.Content()
 			if err != nil {
 				return err
 			}
 
+			fmt.Println("Output config determined")
+			PrintMemUsage()
 			exp, err := exportertfactory.NewExporter(logger, outputCfg)
 			if err != nil {
 				return err
 			}
 
+			fmt.Println("After exporter is created")
+			PrintMemUsage()
 			ser, err := in.Read(ctx, series.Params{
 				Matchers: matchers,
 				MinTime:  timestamp.Time(mint.PrometheusTimestamp()),
@@ -76,6 +104,8 @@ func registerExport(m map[string]setupFunc, app *kingpin.Application) {
 				return err
 			}
 
+			fmt.Println("After series reader iterator is created")
+			PrintMemUsage()
 			df, err := dataframe.FromSeries(ser, *resolution, func(o *dataframe.AggrsOptions) {
 				// TODO(inecas): Expose the enabled aggregations via flag.
 				o.Count.Enabled = true
@@ -83,6 +113,9 @@ func registerExport(m map[string]setupFunc, app *kingpin.Application) {
 				o.Min.Enabled = true
 				o.Max.Enabled = true
 			})
+
+			fmt.Println("after dataframe iterator is created")
+			PrintMemUsage()
 			if err != nil {
 				return errors.Wrap(err, "dataframe creation")
 			}
@@ -90,10 +123,14 @@ func registerExport(m map[string]setupFunc, app *kingpin.Application) {
 			if *dbgOut {
 				dataframe.Print(os.Stdout, df)
 			}
+			fmt.Println("After printing and errors")
+			PrintMemUsage()
 
 			if err := exp.Export(ctx, df); err != nil {
 				return errors.Wrapf(err, "export dataframe")
 			}
+			fmt.Println("After export")
+			PrintMemUsage()
 			return nil
 		}, func(error) { cancel() })
 		return nil
